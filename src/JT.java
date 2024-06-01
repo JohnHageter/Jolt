3b142b541935fa8bdab6df4b2458d908dc25c92b
JohnHageter <hageterj@gmail.com>
2024-06-01 15:16:47 -0400

    Rename Test class to JT

--- src/JT.java
+++ src/JT.java
@@ -1,11 +1,10 @@
 import ij.*;
 import CalciumImaging.*;
-import DacksLab.*;
 import ij.plugin.frame.RoiManager;
 
 import java.io.File;
 
-public class toplevel_testIJ {
+public class JT {
 
     public static void main(String[] args){
         String cellRois = "resources/CellMap_rois.zip";
